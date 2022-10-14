5de9ac15e629909b664b432022309cf6b6e63ac7
tosel3 <45706846+tosel3@users.noreply.github.com>
2022-10-14 14:59:50 -0500

    finished task2

--- task1.py
+++ task1.py
@@ -2,12 +2,25 @@ import cv2
 
 # To read image from disk, we use
 # cv2.imread function, in below method,
-img = cv2.imread("geeksforgeeks.png", cv2.IMREAD_COLOR)
+# TASK 1: img = cv2.imread("geeksforgeeks.png", cv2.IMREAD_COLOR)
+
+	
+img = cv2.imread("maxresdefault.jpg", cv2.IMREAD_COLOR)
  
+
+
+
+
+# Drawing some rectangle
+cv2.rectangle(img, (10, 10), (100, 100), (255, 0, 0), 3)
+cv2.rectangle(img, (120, 120), (150, 150), (255, 0, 0), 5)
+cv2.rectangle(img, (200, 300), (962, 457), (255, 0, 0), -1)
+
+
 # Creating GUI window to display an image on screen
 # first Parameter is windows title (should be in string format)
 # Second Parameter is image array
-cv2.imshow("image", img)
+cv2.imshow('image', img)
  
 # To hold the window on screen, we use cv2.waitKey method
 # Once it detected the close input, it will release the control
@@ -15,8 +28,13 @@ cv2.imshow("image", img)
 # First Parameter is for holding screen for specified milliseconds
 # It should be positive integer. If 0 pass an parameter, then it will
 # hold the screen until user close it.
-cv2.waitKey(0)
+#       cv2.waitKey(0)
  
 # It is for removing/deleting created GUI window from screen
 # and memory
+#           cv2.destroyAllWindows()
+
+# To finalize, we will wait for the user to press any key on the keyboard and, when that happens, destroy the window and end the program.
+
+cv2.waitKey(0)
 cv2.destroyAllWindows()
